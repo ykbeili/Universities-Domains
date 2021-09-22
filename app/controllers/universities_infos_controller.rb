404a19d0c12288774f18a9b006847e44f0b12f9a
Yousef Kbeili <yousef@curbie.ca>
2021-09-21 21:01:31 -0600

    Add view and route for univsersities infos

--- app/controllers/universities_infos_controller.rb
+++ app/controllers/universities_infos_controller.rb
@@ -1,2 +1,17 @@
 class UniversitiesInfosController < ApplicationController
+  require 'uri'
+  require 'net/http'
+  before_action :check_new_universities
+
+  def index
+    uri = URI('http://universities.hipolabs.com/search?country=united+states')
+    res = Net::HTTP.get_response(uri)
+    UniversitiesInfo::LIST_OF_COUNTRIES
+  end
+
+  private
+
+  def check_new_universities
+    
+  end
 end
--- app/models/universities_info.rb
+++ app/models/universities_info.rb
@@ -1,2 +1,3 @@
 class UniversitiesInfo < ApplicationRecord
+  LIST_OF_COUNTRIES = %w[united\ states canada].freeze
 end
